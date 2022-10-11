bedd6586e3be9178d9b061cb7eac40653b936853
alexandreb247 <alexandre@poczta.onet.pl>
2022-10-11 11:10:55 +0200

    zadanie wykonane

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function dowolneLiczby(a, b){
+      if (a == 20 || b ==20 || a+b <= 20 ) {
+        return console.log(Boolean(true));
+    } else {
+        return console.log(Boolean(false));
+   }
+}
+dowolneLiczby(3,17);
+dowolneLiczby(20,5); 
+dowolneLiczby(1,111);
+dowolneLiczby(0,20);
+dowolneLiczby(0,-3);
+
